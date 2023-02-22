ad04bfb3264aa4b7ca3843fc6a5c43d0360210ea
Krunal Patel <krunalkachadia@gmail.com>
2023-02-22 05:05:59 -0500

    Create la.py

---
+++ la.py
@@ -0,0 +1,26 @@
+# All necessary import functions
+import numpy as np
+from numpy import linalg as LA
+
+matrix_A = np.array([[1, 3, 1],
+                    [1, 2, 5],
+                    [1, 7, 0]])
+
+matrix_B = np.array([[2, 4, 6],
+                    [2, 4, 6],
+                    [2, 4, 6]])
+
+# Add two matrix
+matrix_A + matrix_B
+
+# Find inverse of the following matrix
+mat_A = np.array([[1, 0, 1, 2], 
+                  [-1, 1, 2, 0], 
+                  [-2, 0, 1, 2], 
+                  [0, 0, 0, 1]])
+
+# Find eigenvectors and eignevalues of the following matrix
+matrix_A = np.array([[4, -3, -3],
+                    [3, -2, -3],
+                    [-1, 1, 2]])
+
